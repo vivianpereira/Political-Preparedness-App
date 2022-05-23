abfdbed6b04f90cde6185e5abbc786f1558b2620
Vivian Pereira <47736151+vivianpereira@users.noreply.github.com>
2022-05-23 14:55:31 +1000

    Creating election screen  (#10)
    * Implementing election screen, display the first recyclerView and navigates to voter info screen.
    
    * Addressing code review comments.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -1,11 +1,21 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.politicalpreparedness.network.models.Division
+import com.example.android.politicalpreparedness.network.models.Election
+import java.util.*
 
 //TODO: Construct ViewModel and provide election datasource
-class ElectionsViewModel: ViewModel() {
+class ElectionsViewModel : ViewModel() {
 
     //TODO: Create live data val for upcoming elections
+    private val _upcomingElectionsList = MutableLiveData<List<Election>>(
+        mutableListOf(createFakeElection())
+    )
+    val upcomingElectionsList: LiveData<List<Election>>
+        get() = _upcomingElectionsList
 
     //TODO: Create live data val for saved elections
 
@@ -13,4 +23,15 @@ class ElectionsViewModel: ViewModel() {
 
     //TODO: Create functions to navigate to saved or upcoming election voter info
 
+
+    private fun createFakeElection() = Election(
+        id = 1,
+        division = Division(
+            id = "1",
+            country = "Australia",
+            state = "QLD"
+        ),
+        electionDay = Date(),
+        name = "Election 1"
+    )
 }
