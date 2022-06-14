f5dfa035e1497bf6c387c8d8a4a7ed54fbb6f686
vivianazevedopereira <vivispereira.21@gmail.com>
2022-06-14 15:49:16 +1000

    Implementing Representative layout screen, strings and styles.

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.*
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.databinding.FragmentRepresentativeBinding
 import com.example.android.politicalpreparedness.network.models.Address
 import java.util.Locale
 
@@ -23,13 +24,15 @@ class DetailFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         //TODO: Establish bindings
+        val binding = FragmentRepresentativeBinding.inflate(inflater)
+        binding.lifecycleOwner = this
 
         //TODO: Define and assign Representative adapter
 
         //TODO: Populate Representative adapter
 
         //TODO: Establish button listeners for field and location search
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return binding.root
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
